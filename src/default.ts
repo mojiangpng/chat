4406568cffc1166e21d5cc09742294edc568d30b
mojiangpng <129439469+mojiangpng@users.noreply.github.com>
2023-03-31 04:32:08 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
+- 本站免费使用公共的KEY，如有需要可填入自己的 key 。
 - 本站功能强大，响应速度快，欢迎自部署。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。
